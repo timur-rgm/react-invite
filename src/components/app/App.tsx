b23aa9858ae68687284744721f2efe9c3d111dc5
Timur <tiabumuaz@gmail.com>
2022-12-03 13:50:14 +0500

    Добавлено состояние для значения поля ввода. Реализована логика его обновления

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,11 @@
-import {useState} from 'react';
+import {useState, ChangeEvent} from 'react';
 import Success from '../success/Success';
 import UsersList from '../users-list/Users-list';
 
 function App() {
   const [users, setUsers] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
 
   fetch('https://6353e24dccce2f8c02fe8dcd.mockapi.io/users')
     .then((response) => response.json())
@@ -12,11 +13,17 @@ function App() {
     .catch((err) => console.log(err))
     .finally(() => setLoading(false));
 
+  const onSearchValueChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(evt.target.value);
+  };
+
   return (
     <div className="App">
       <UsersList
         users={users}
         isLoading={isLoading}
+        searchValue={searchValue}
+        onSearchValueChange={onSearchValueChange}
       />
       {/* <Success /> */}
     </div>
--- src/components/users-list/Users-list.tsx
+++ src/components/users-list/Users-list.tsx
@@ -1,3 +1,4 @@
+import {ChangeEvent} from 'react';
 import Skeleton from '../skeleton/Skeleton';
 import User from '../user/User';
 import {UsersType} from '../../types/users';
@@ -5,9 +6,16 @@ import {UsersType} from '../../types/users';
 type UsersListType = {
   isLoading: boolean,
   users: UsersType,
+  searchValue: string,
+  onSearchValueChange: (evt: ChangeEvent<HTMLInputElement>) => void,
 }
 
-function UsersList({isLoading, users}: UsersListType): JSX.Element {
+function UsersList({
+  isLoading,
+  users,
+  searchValue,
+  onSearchValueChange,
+}: UsersListType): JSX.Element {
   
   return (
     <>
@@ -15,7 +23,13 @@ function UsersList({isLoading, users}: UsersListType): JSX.Element {
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        
+        <input
+          value={searchValue}
+          onChange={onSearchValueChange}
+          type="text"
+          placeholder="Найти пользователя..."
+        />
       </div>
 
       {
