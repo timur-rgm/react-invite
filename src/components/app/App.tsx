f7f01a729554e2b531828f49e79dc09ae31577e3
Timur <tiabumuaz@gmail.com>
2022-12-03 12:18:38 +0500

    Добавлены состояние users и запись в него загруженных пользователей

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,15 @@
+import {useState} from 'react';
 import Success from '../success/Success';
 import UsersList from '../users-list/Users-list';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  fetch('https://6353e24dccce2f8c02fe8dcd.mockapi.io/users')
+    .then((response) => response.json())
+    .then((users) => setUsers(users))
+    .catch((err) => console.log(err));
+
   return (
     <div className="App">
       <UsersList isLoading={false} />
